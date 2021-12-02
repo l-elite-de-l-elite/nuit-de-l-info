0a153bbc14c743a8fef721332e648c70b2e0d421
matthieurobert <matthieurobert82@gmail.com>
2021-12-02 17:35:17 +0100

    Init databse connection

--- dunkerke_sauvetage/start/routes.ts
+++ dunkerke_sauvetage/start/routes.ts
@@ -18,8 +18,17 @@
 |
 */
 
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
+})
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy
+    ? response.ok(report)
+    : response.badRequest(report)
 })
